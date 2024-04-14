1a4ca0a045dc5f88f81c12f5a5820bdae42038c6
LeugiMH <miguelhenriquegs369@gmail.com>
2024-04-14 17:21:07 -0300

    ADD: Definindo limites

--- testeCoordenadas.php
+++ testeCoordenadas.php
@@ -23,8 +23,8 @@
         * that contains the map. 
         */
         #map {
-        height: 100%;
-        width: 100%;
+        height: 50%;
+        width: 50%;
         }
 
         /* 
@@ -41,23 +41,50 @@
 </head>
 <body>
 
+    <form action="" method="post">
+        <input type="button" id="btn teste" value="Botão Teste">
+    </form>
     <div id="map"></div>
+
         
 
         <!-- prettier-ignore -->
     <script>
         (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: <?php echo "\"$MapsAPIKey\"";?>, v: "weekly"});
+        ({key: "<?php echo $MapsAPIKey;?>", v: "weekly"});
     </script>
     <script>
         let map;
-
+        const margem = {norte: -23.332912, leste: -46.719139, sul: -23.338775, oeste: -46.725957};
+        console.log(margem.norte);
         async function initMap() {
         const { Map } = await google.maps.importLibrary("maps");
 
+        console.log(Map);
         map = new Map(document.getElementById("map"), {
-            center: { lat: -23.336286, lng: -46.722118 },
-            zoom: 15,
+            zoom:10,
+            //-23.335872, -46.722554
+            center: { lat: -23.335872, lng: -46.722554},
+            mapTypeId: 'satellite',
+            disableDefaultUI: false,
+            zoomControl: true,
+            mapTypeControl: true,
+            scaleControl: true,
+            streetViewControl: true,
+            rotateControl: true,
+            fullscreenControl: true,
+            //Restrições
+            restriction: { 
+                strictBounds: true,
+                //-23.332912, -46.725957
+                //-23.338775, -46.719139
+                latLngBounds: { //Limitar as Bordas 
+                    north: margem.norte,
+                    south: margem.sul,
+                    east: margem.leste,
+                    west: margem.oeste,
+                },
+            },
         });
         }
         initMap();

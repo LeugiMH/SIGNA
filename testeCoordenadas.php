e0ce02c34c3ce33650a3472fdd2f13d009249aa6
LeugiMH <miguelhenriquegs369@gmail.com>
2024-08-10 18:44:22 -0300

    upd teste mapa

--- testeCoordenadas.php
+++ testeCoordenadas.php
@@ -86,9 +86,143 @@
                     },
                 },
             });
+
+            // Colocando a imagem no mapa
+            // Definindo posição do mapa
+            //-23.335590, -46.722704
+            //-23.336609, -46.721262
+            const bounds = new google.maps.LatLngBounds(
+                new google.maps.LatLng(-23.335590, -46.722704),
+                new google.maps.LatLng(-23.336609, -46.721262)
+            );
+            // The photograph is courtesy of the U.S. Geological Survey.
+            let image = "https://developers.google.com/maps/documentation/javascript/examples/full/images/talkeetna.png";
+
+            console.log(bounds);
+            /**
+             * The custom USGSOverlay object contains the USGS image,
+             * the bounds of the image, and a reference to the map.
+             */
+            class USGSOverlay extends google.maps.OverlayView {
+                bounds;
+                image;
+                div;
+                constructor(bounds, image) {
+                super();
+                this.bounds = bounds;
+                this.image = image;
+                }
+                /**
+                 * onAdd is called when the map's panes are ready and the overlay has been
+                 * added to the map.
+                 */
+                onAdd() {
+                this.div = document.createElement("div");
+                this.div.style.borderStyle = "none";
+                this.div.style.borderWidth = "0px";
+                this.div.style.position = "absolute";
+
+                // Create the img element and attach it to the div.
+                const img = document.createElement("img");
+
+                img.src = this.image;
+                img.style.width = "100%";
+                img.style.height = "100%";
+                img.style.position = "absolute";
+                this.div.appendChild(img);
+
+                // Add the element to the "overlayLayer" pane.
+                const panes = this.getPanes();
+
+                panes.overlayLayer.appendChild(this.div);
+                }
+                draw() {
+                // We use the south-west and north-east
+                // coordinates of the overlay to peg it to the correct position and size.
+                // To do this, we need to retrieve the projection from the overlay.
+                const overlayProjection = this.getProjection();
+                // Retrieve the south-west and north-east coordinates of this overlay
+                // in LatLngs and convert them to pixel coordinates.
+                // We'll use these coordinates to resize the div.
+                const sw = overlayProjection.fromLatLngToDivPixel(
+                    this.bounds.getSouthWest()
+                );
+                const ne = overlayProjection.fromLatLngToDivPixel(
+                    this.bounds.getNorthEast()
+                );
+
+                // Resize the image's div to fit the indicated dimensions.
+                if (this.div) {
+                    this.div.style.left = sw.x + "px";
+                    this.div.style.top = ne.y + "px";
+                    this.div.style.width = ne.x - sw.x + "px";
+                    this.div.style.height = sw.y - ne.y + "px";
+                }
+                }
+                /**
+                 * The onRemove() method will be called automatically from the API if
+                 * we ever set the overlay's map property to 'null'.
+                 */
+                onRemove() {
+                if (this.div) {
+                    this.div.parentNode.removeChild(this.div);
+                    delete this.div;
+                }
+                }
+                /**
+                 *  Set the visibility to 'hidden' or 'visible'.
+                 */
+                hide() {
+                if (this.div) {
+                    this.div.style.visibility = "hidden";
+                }
+                }
+                show() {
+                if (this.div) {
+                    this.div.style.visibility = "visible";
+                }
+                }
+                toggle() {
+                if (this.div) {
+                    if (this.div.style.visibility === "hidden") {
+                    this.show();
+                    } else {
+                    this.hide();
+                    }
+                }
+                }
+                toggleDOM(map) {
+                if (this.getMap()) {
+                    this.setMap(null);
+                } else {
+                    this.setMap(map);
+                }
+                }
+            }
+            const overlay = new USGSOverlay(bounds, image);
+
+            overlay.setMap(map);
+
+            const toggleButton = document.createElement("button");
+
+            toggleButton.textContent = "Toggle";
+            toggleButton.classList.add("custom-map-control-button");
+
+            const toggleDOMButton = document.createElement("button");
+
+            toggleDOMButton.textContent = "Toggle DOM Attachment";
+            toggleDOMButton.classList.add("custom-map-control-button");
+            toggleButton.addEventListener("click", () => {
+            overlay.toggle();
+            });
+            toggleDOMButton.addEventListener("click", () => {
+            overlay.toggleDOM(map);
+            });
+            map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleDOMButton);
+            map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleButton);
+        
         }
         initMap();
-
         var habMarcador = null;
         function habilitarMarcador()
         {
@@ -113,7 +247,6 @@
             
             console.log("adicionarMarcador-Fim");
         }
-
     </script>  
 </body>
 </html>

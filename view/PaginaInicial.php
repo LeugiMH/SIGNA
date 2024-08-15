578a4a4aa98572b59bf918be0b600ffa10eba971
LeugiMH <miguelhenriquegs369@gmail.com>
2024-08-15 00:26:56 -0300

    Ajustes mapa

--- view/PaginaInicial.php
+++ view/PaginaInicial.php
@@ -5,7 +5,7 @@
     <title>SIGNA</title>
     <style>
 
-    #map { height: 1000px; }
+    #map { height: 500px; z-index: 100; }
     </style>
 </head>
 <body>
@@ -13,56 +13,61 @@
     <?php include_once "resource/navbarControle.php";?>
 
     <div class="corpo h-100">
-        <div class="conteudo bg-secondary">
-            <div class="container-fluid p-0 folhas min-vh-100">
+        <div class="conteudo bg-secondary h-100">
+            <div class="container-fluid folhas1 pt-5 m-0 row justify-content-center align-content-center" style="min-height: 50vh;">
                 <!--Container de conteúdo-->
-
-                <div class="bg-light">
-                    <h1>aa</h1>
+                <div class="col-lg-6">
+                    <div id="map"></div>
                 </div>
-                <div id="map"></div>
-                
+            </div>
+            <div class="container-fluid folhas2 p-3 m-0 row justify-content-center align-content-center position-relative" style="min-height: 50vh;">
+                <img src="<?php echo URL.'resource/ui/bg/bg_nuvem_icon.svg'?>" class="nuvem px-0 mt-0">
+                <div class="col-lg-6">
+                <h1>aa</h1>
+                </div>
             </div>
         </div>
-        <div class="rodape bg-dark text-white row m-0">
-            <div class="col py-4 text-center">
-                <h3 class="m-0" id="teste">RODAPÉ</h3>
+        <div class="rodape bg-dark text-white h-100">
+            <div class="py-4 text-center row m-0 p-0">
+                <h3 class="m-0 p-0 col" id="teste_coord">RODAPÉ</h3>
             </div>
         </div>
     </div>
     <script>
+        //Exibir Mapa
         var map = L.map('map').setView([40.712216, -74.22655], 21);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
-//Overlay GNOMO
+        //Overlay Imagem
         var imageUrl = 'https://blog.yurimotatech.com/wp-content/uploads/2022/02/Hello-World-Python.png',
             imageBounds = [[40.712216, -74.22655], [40.773941, -74.12544]];
             L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-            var myIcon = L.icon({
-                iconUrl: 'ui/bg/arvore.png',
-                iconSize: [38, 95],
-                iconAnchor: [22, 94],
-                popupAnchor: [-3, -76],
-                shadowUrl: 'ui\bg\arvore.png',
-                shadowSize: [68, 95],
-                shadowAnchor: [22, 94]
-            });
+        //Alterar ìcone do Marker
+        var myIcon = L.icon({
+            iconUrl: '<?php echo URL.'resource/ui/bg/arvore.png'?>',
+            iconSize: [30, 30],
+            iconAnchor: [15, 30],
+            /*popupAnchor: [-3, -76],
+            shadowUrl: 'ui\bg\arvore.png',
+            shadowSize: [68, 95],
+            shadowAnchor: [22, 94]*/
+        });
 
+        //Exibir um marcador ao clicar
         function onMapClick(e) {
             /*popup
             .setLatLng(e.latlng)
             .setContent("You clicked the map at " + e.latlng.toString())
             .openOn(map);*/
             L.marker(e.latlng, {icon: myIcon}).addTo(map);
-            var teste = document.getElementById("teste");
+            var teste = document.getElementById("teste_coord");
             teste.innerHTML = e.latlng;
         }
-
-map.on('click', onMapClick);
+        map.on('click', onMapClick);
 
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- view/resource/navbarUser.php
+++ view/resource/navbarUser.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-warning">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#" aria-label="SIGNA, Sistema de Gerenciamento de Núcleo Arbóreo">SIGNA</a>
+        <a class="navbar-brand" href="inicio" aria-label="SIGNA, Sistema de Gerenciamento de Núcleo Arbóreo">SIGNA</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">FATEC Franco da Rocha</a>
+                    <a class="nav-link" href="https://fatecfrancodarocha.edu.br/" target="_blank">FATEC Franco da Rocha</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Features</a>

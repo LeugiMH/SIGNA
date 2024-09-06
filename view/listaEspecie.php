0d8d27a97715507e9e46fc03a658d6679221c55f
LeugiMH <miguelhenriquegs369@gmail.com>
2024-09-06 20:05:48 -0300

    Lista responsiva

--- view/listaEspecie.php
+++ view/listaEspecie.php
@@ -16,7 +16,7 @@
                     <!-- Conteúdo -->
                     <h1 class="display-1 text-center mb-5">ESPÉCIES</h1>
                     <div class="bg-verde p-3 p-lg-5 rounded-4 text-white">
-                        <table id="tabela" class="table table-striped" style="width:100%">
+                        <table id="lista" class="table table-striped" style="white-space: nowrap;">
                             <thead>
                                 <tr>
                                     <th>IDESPÉCIE</th>
@@ -75,22 +75,30 @@
         </div>
         <?php include_once "resource/rodape.php";?>
     </div>
+
+    <!---->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
-    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.datatables.net/2.1.4/js/dataTables.js"></script>
-    <script src="https://cdn.datatables.net/2.1.4/js/dataTables.bootstrap5.js"></script>
+
+
+    <script src= "https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src= "https://cdn.datatables.net/2.1.5/js/dataTables.js"></script>
+    <script src= "https://cdn.datatables.net/responsive/3.0.3/js/dataTables.responsive.js"></script>
+    <script src= "https://cdn.datatables.net/responsive/3.0.3/js/responsive.dataTables.js"></script>
+
     <script>
     $(document).ready(function() {
-        $('#tabela').DataTable({
+        $('#lista').DataTable({
+            responsive: true,
             language: {
                 url: "<?php echo URL.'resource/json/pt_br.json';?>"
             }
         });
     });
     </script>
+
+
 </body>
 
 </html>

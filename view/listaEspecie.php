d608e9d9ae098c8ab66cfa03b89369fa108c960f
LeugiMH <miguelhenriquegs369@gmail.com>
2024-09-06 22:39:10 -0300

    Deixando bibliotecas locais

--- view/listaEspecie.php
+++ view/listaEspecie.php
@@ -75,13 +75,9 @@
         </div>
         <?php include_once "resource/rodape.php";?>
     </div>
-
-	<!--Bootstrap-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    
-    <?php include_once "resource/pluginsDataTables.html";?>
+	<!--Imports-->  
+    <?php include_once "resource/plugins.php";?>
+    <?php include_once "resource/pluginsDataTables.php";?>
 </body>
 
 </html>

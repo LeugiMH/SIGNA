ab04ea5ed48473168d6d78c36663c88ac3650ac3
valimnathaly <valimnathaly@outlook.com>
2024-09-30 22:04:00 -0300

    FFEDBACK

---
+++ controller/feedback.php
@@ -0,0 +1,158 @@
+<?php
+include_once "model/feedback.php";
+
+class FeedbackController
+{
+    //Cadastrar
+    function enviarFeedback()
+    {
+        $email =  $_POST["inputEmail"];
+        $rating =  $_POST["rating"];
+        $assunto =  $_POST["inputAssunto"];
+        $comentario =  $_POST["inputComentario"];
+
+
+        //Cria objeto da classe espécie e define valores
+        $cmd = new Feedback();
+        $cmd->IDFEEDBACK      = $nomeCie;
+        $cmd->IDESPECIME      = $nomePop;
+        $cmd->TPUSUARIO      = $nomePop;
+        $cmd->AVALIACAO      = $Familia;
+        $cmd->IDASSUNTO      = $habitat;
+        $cmd->TEXTO       = $altura;
+        $cmd->EMAIL = $ImgDesc;
+        $cmd->DATACAD      = date("d-m-Y h:i:s"); //Data atual de cadastro
+        $cmd->IDADMIN     = $_SESSION["sessaoLogada"]->IDADMIN;
+        COMENT_ADMIN //Id do administrador logado
+        
+        $novoNome = "";
+        $nomeTemp = "";
+        
+
+        if($cmd->cadastrar())  //Sucesso ao cadastrar espécie
+        {
+            setcookie("msg","<div class='alert alert-success'>Espécie cadastrada com sucesso</div>",time() + 1,"/");
+
+            //Mover imagem para pastas no servidor
+            $pastaDestino = "resource/imagens/especies/$novoNome";   //pasta destino
+            move_uploaded_file($nomeTemp, $pastaDestino);       //mover o arquivo 
+        }
+        else
+        {
+            setcookie("msg","<div class='alert alert-danger'>Erro ao cadastrar espécie</div>",time() + 1,"/");
+        }
+        header("location: ".URL."especies/cadastro");
+    }
+
+    //Consultar
+    function buscar($id)
+    {
+        $especie = new Especie();
+        $especie->IDESPECIE = $id;
+        return $especie->buscar();
+    }
+
+    //Listar
+    function listar()
+    {
+        $cmd = new Especie();
+        return $cmd->listar();
+    }
+
+    //Alterar
+    function alterarEspecie()
+    {
+        $idEspecie =  $_POST["inputId"];
+        $nomeCie =  $_POST["inputNomeCie"];
+        $nomePop =  $_POST["inputNomePop"];
+        $Familia =  $_POST["inputFamilia"];
+        $habitat =  $_POST["inputHabitat"];
+        $altura  =  $_POST["inputAltura"];
+        $ImgDesc =  $_POST["inputImgDesc"];
+        
+        //Cria objeto da classe espécie e define valores
+        $cmd = new Especie();
+        $cmd->IDESPECIE    = $idEspecie;
+        $cmd->NOMECIE      = $nomeCie;
+        $cmd->NOMEPOP      = $nomePop;
+        $cmd->FAMILIA      = $Familia;
+        $cmd->HABITAT      = $habitat;
+        $cmd->ALTURA       = $altura;
+        $cmd->DESCRICAOIMG = $ImgDesc;
+        
+        $busca = new Especie();
+        $busca->IDESPECIE = $idEspecie;
+        $dadosEspecie = $busca->buscar();
+
+        $nomeTemp = "";
+        $novoNome = "";
+        //Caso imagem seja enviada sem erros
+        if($_FILES["inputImagem"]["error"] == 0)
+        {
+            //Espécie já possuia uma imagem
+            if(isset($dadosEspecie->IMAGEM))
+            {
+                unlink("resource/imagens/especies/$dadosEspecie->IMAGEM"); //excluir a imagem
+            }
+            $nomeArquivo = $_FILES["inputImagem"]["name"];       //Nome do arquivo
+            $nomeTemp =    $_FILES["inputImagem"]["tmp_name"];      //nome temporário
+            
+            //pegar a extensão do arquivo
+            $info = new SplFileInfo($nomeArquivo);
+            $extensao = $info->getExtension();
+            
+            if($extensao != "jpg" && $extensao != "png" && $extensao != "jpeg" && $extensao != "webp")
+            {
+                setcookie("msg","<div class='alert alert-danger'>Imagem deve ter a extensão .JPG, .JPEG, .PNG, ou .WEBP.</div>",time() + 1,"/");
+                header("location: ".URL."especies/altera/$idEspecie");
+                return;
+            }
+            //gerar novo nome
+            $novoNome = md5(microtime()) . ".$extensao";
+            
+            //enviando para o banco
+            $cmd->IMAGEM = $novoNome;
+        }
+        else //Caso não seja enviada mantém imagem original
+        {
+            $cmd->IMAGEM = $dadosEspecie->IMAGEM;
+        }
+
+        if($cmd->alterar()) //Sucesso ao alterar espécie
+        {
+            //Mover imagem para pastas no servidor
+            $pastaDestino = "resource/imagens/especies/$novoNome";   //pasta destino
+            move_uploaded_file($nomeTemp, $pastaDestino);       //mover o arquivo 
+
+            header("location: ".URL."especies/lista");
+        }
+        else
+        {
+            setcookie("msg","<div class='alert alert-danger'>Erro ao alterar espécie</div>",time() + 1,"/");
+            header("location: ".URL."especies/altera/$idEspecie");
+        }
+    }
+
+    //Excluir
+    function excluirEspecie($id)
+    {
+        $idEspecie = $id;
+
+        $cmd = new Especie();
+        $cmd->IDESPECIE = $idEspecie;
+
+        $especie = $cmd->buscar();
+
+        if($cmd->excluir())
+        {
+            unlink("resource/imagens/especies/$especie->IMAGEM"); //Excluir imagem
+        }
+        else
+        {
+            setcookie("msgLista","<div class='alert alert-danger'>Erro ao excluir a espécie, é possível que essa espécie possua algum espécime relacionado.</div>",time() + 1,"/");
+        }
+        header("location: ".URL."especies/lista");
+    }
+}
+
+?>
--- view/resource/rodapeUser.php
+++ view/resource/rodapeUser.php
@@ -26,19 +26,79 @@
 <!-- Modal -->
 <div class="modal fade" id="feedback" tabindex="-1" aria-labelledby="FeedBack PopUp page" aria-hidden="true">
   <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-content bg-verde">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Enviar Feedback</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <form action="enviarFeedback" method="POST">
+            <?php
+                //Exibindo mensagem de erro
+                if(isset($_COOKIE["msg"]))
+                {echo $_COOKIE['msg'];}
+            ?>
+            <div class="mb-3">
+                <label for="inputEmail" class="form-label">Endereço de Email</label>
+                <input type="email" value="" class="form-control" id="inputEmail" name="inputEmail" aria-label="Digite o email (opcional)" maxlength="256">
+            </div>
+            <div class="mb-3">
+                <label for="rating" class="form-label">Avaliação</label>
+                <div class="rating">
+                    <img class="rating__star star-i star me-2"></img>
+                    <img class="rating__star star-i star me-2"></img>
+                    <img class="rating__star star-i star me-2"></img>
+                    <img class="rating__star star-i star me-2"></img>
+                    <img class="rating__star star-i star me-2"></img>
+              </div>
+              <input type="hidden" value="" class="form-control" id="rating" name="rating">
+            </div>
+            <div class="mb-3">
+              <select name="inputAssunto" id="inputAssunto" class="form-select" aria-label="Selecione um assunto para enviar o feedback" required>
+                <option disabled selected>Selecione uma Espécie</option> 
+                <?php
+                foreach ($assuntos as $assunto)
+                {
+                  echo"<option value='$assunto->IDASSUNTO'>$assunto->DESCRICAO</option>";
+                }
+                ?>
+              </select>
+            </div>
+            <div class="d-flex justify-content-between">
+                <button type="button" class="btn btn-success" onclick="history.back()">Voltar</button>
+                <button type="submit" class="btn btn-success">Logar</button>
+            </div>
+        </form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary">Enviar Feedback</button>
       </div>
     </div>
   </div>
 </div>
 </footer>
+
+<script>
+  const ratingStars = [...document.getElementsByClassName("rating__star")];
+
+  function executeRating(stars) {
+    const starClassActive = "rating__star star-a star me-2";
+    const starClassInactive = "rating__star star-i star me-2";
+    const starsLength = stars.length;
+    let i;
+    stars.map((star) => {
+      star.onclick = () => {
+        document.getElementById("rating").value = stars.indexOf(star)+1;
+        i = stars.indexOf(star);
+
+        if (star.className===starClassInactive) {
+          for (i; i >= 0; --i) stars[i].className = starClassActive;
+        } else {
+          for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+        }
+      };
+    });
+  }
+
+  executeRating(ratingStars);
+</script>

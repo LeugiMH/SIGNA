6126b577d618f80592f74179fbc474427c7a259e
Drynee <carolinamattiazzoreder@gmail.com>
2024-10-18 21:34:10 -0300

    fix url

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
 date_default_timezone_set("America/Sao_Paulo");
 
 //Definindo uma constante para a URL do site
-define("URL","http://localhost/SIGNA-1/");
+define("URL","http://localhost/SIGNA/");
 if($_GET)
 {
     //Pegando a URL e apagando a "/" no final dela.
